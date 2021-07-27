39614296c2fea66006cbaa7d72d1d802bda3e1cc
RisingMobi <RisingMobi@mail.com>
2021-07-27 09:52:44 +0300

    feat(RGSMSD-42): Android / Кнопки
    Removed log

--- app/src/main/java/com/custom/rgs_android_dom/ui/demo/DemoInputsFragment.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/demo/DemoInputsFragment.kt
@@ -1,18 +1,11 @@
 package com.custom.rgs_android_dom.ui.demo
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.custom.rgs_android_dom.R
-import com.custom.rgs_android_dom.databinding.FragmentDemoBinding
 import com.custom.rgs_android_dom.databinding.FragmentDemoInputsBinding
-import com.custom.rgs_android_dom.databinding.FragmentDemoRegistrationFlowBinding
 import com.custom.rgs_android_dom.ui.base.BaseFragment
-import com.custom.rgs_android_dom.ui.navigation.REGISTRATION
-import com.custom.rgs_android_dom.ui.navigation.ScreenManager
-import com.custom.rgs_android_dom.ui.registration.phone.RegistrationPhoneFragment
 import com.custom.rgs_android_dom.utils.formatTo
-import com.custom.rgs_android_dom.utils.setOnDebouncedClickListener
 import com.custom.rgs_android_dom.utils.showDatePicker
 import com.custom.rgs_android_dom.views.edit_text.MSDEditText
 import com.custom.rgs_android_dom.views.edit_text.MSDLabelEditText
@@ -23,7 +16,6 @@ class DemoInputsFragment: BaseFragment<DemoViewModel, FragmentDemoInputsBinding>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("MyLog", "On view created")
         binding.labelTextView.setValue("Иванов Иван Русланович")
 
         binding.labelIconTextView.setIcon(R.drawable.ic_mastercard)
