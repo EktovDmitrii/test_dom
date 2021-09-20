eedd98b5d0892998ae7bffe61071cbec3d669584
RisingMobi <RisingMobi@mail.com>
2021-09-20 20:16:14 +0300

    fix(RGSMSD-328): Перевернусь последовательность сообщений в чате
    resolved issue

--- app/src/main/java/com/custom/rgs_android_dom/data/repositories/chat/ChatRepositoryImpl.kt
+++ app/src/main/java/com/custom/rgs_android_dom/data/repositories/chat/ChatRepositoryImpl.kt
@@ -18,105 +18,12 @@ class ChatRepositoryImpl(private val api: MSDApi,
         val client = authSharedPreferences.getClient()
         val channelId = client?.getChatChannelId() ?: ""
 
-        return api.getChatMessages(channelId, 20, 0).map {
+        return api.getChatMessages(channelId, 1000, 0).map {
             ChatMapper.responseToChatMessages(it, client?.userId ?: "")
+        }.map {
+            it.reversed()
         }
 
-//        return Single.fromCallable {
-//            val messages = arrayListOf<ChatMessageModel>()
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Добрый день!",
-//                    sender= ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Я мастер онлайн",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Подскажите, чем я могу вам помочь?",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Добрый день, я хотел проконсультироваться у вас по вопросу вызова мастера на дом",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Что именно вас интересует",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "У меня течет кран, затопило всю квартиру, соседи ломятся в дверь",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Для начала вам надо перекрыть воду",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Я не знаю как это сделать",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Найдите люк в котором находятся ваши счестчики воды",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "О, да, тут есть 2 кранчика",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Отлично поверните оба",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Да, все получилось, у меня больше не бежит вода",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Отлично, теперь я могу вызвать вам на дом мастера, который починит ваш кран",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Да, это было бы отлично, можно так же заказать клининг, что бы прибрали здесь все",
-//                    sender = ChatMessageModel.Sender.ME
-//                )
-//            )
-//            messages.add(
-//                ChatMessageModel(
-//                    message = "Конечно, ожидайте, мастер и клининг уже выехали к вам домой",
-//                    sender = ChatMessageModel.Sender.OPPONENT
-//                )
-//            )
-//
-//            return@fromCallable messages
-//        }.delay(3, TimeUnit.SECONDS)
     }
 
     override fun sendMessage(message: String): Completable {
