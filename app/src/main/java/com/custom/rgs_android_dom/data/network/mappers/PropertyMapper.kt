e9561905a0a6a11af79923f2dabd303bf7fc2bfd
RisingMobi <RisingMobi@mail.com>
2021-09-28 11:55:46 +0300

    feat(RGSMSD-342): Добавить все разделы в профиль и сделать к ним заглушечный экран
    Added handling uspecified property type

--- app/src/main/java/com/custom/rgs_android_dom/data/network/mappers/PropertyMapper.kt
+++ app/src/main/java/com/custom/rgs_android_dom/data/network/mappers/PropertyMapper.kt
@@ -7,6 +7,10 @@ import com.custom.rgs_android_dom.domain.property.models.PropertyItemModel
 object PropertyMapper {
 
     fun responseToProperty(response: PropertyItemResponse): PropertyItemModel {
+        val isOwn = if (response.isOwn == "yes") true else if (response.isOwn == "no") false else null
+        val isRent = if (response.isRent == "yes") true else if (response.isRent == "no") false else null
+        val isTemporary = if (response.isTemporary == "yes") true else if (response.isTemporary == "no") false else null
+
         return PropertyItemModel(
             address = response.address,
             clientId = response.clientId,
@@ -15,9 +19,9 @@ object PropertyMapper {
                 PropertyDocument(link = it.link ?: "", name = it.name ?: "")
             } ?: listOf(),
             id = response.id,
-            isOwn = response.isOwn?.equals("yes") == true,
-            isRent = response.isRent?.equals("yes") == true,
-            isTemporary = response.isTemporary?.equals("yes") == true,
+            isOwn = isOwn,
+            isRent = isRent,
+            isTemporary = isTemporary,
             name = response.name,
             status = response.status ?: "",
             totalArea = response.totalArea,
