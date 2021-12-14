2d164b90d1fd5f6019ea5ec484aef32bae47cec8
Влад Сыргашев <v.syrgashev@secreate.io>
2021-12-14 16:39:23 +0300

    feat(RGSMSD-710): Звонок. Поменять видео местами с консультантом

--- app/src/main/java/com/custom/rgs_android_dom/ui/chat/call/CallFragment.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/chat/call/CallFragment.kt
@@ -112,7 +112,7 @@ class CallFragment : BaseFragment<CallViewModel, FragmentCallBinding>(R.layout.f
         }
 
         binding.switchCameraImageView.setOnDebouncedClickListener {
-
+            viewModel.onSwitchCameraClick()
         }
 
         binding.switchSurfacesConsultantImageView.setOnDebouncedClickListener {
@@ -126,7 +126,7 @@ class CallFragment : BaseFragment<CallViewModel, FragmentCallBinding>(R.layout.f
         subscribe(viewModel.callTypeObserver) {
             when (it) {
                 CallType.AUDIO_CALL -> {
-
+                    if (binding.switchCameraImageView.isEnabled) binding.switchCameraImageView.isEnabled = false
                     requestMicPermissionsAction.launch(
                         arrayOf(
                             Manifest.permission.RECORD_AUDIO,
@@ -175,7 +175,7 @@ class CallFragment : BaseFragment<CallViewModel, FragmentCallBinding>(R.layout.f
 
                 }
             }
-
+            binding.switchCameraImageView.isEnabled = roomInfo.cameraEnabled
             binding.micOffImageView.isActivated = roomInfo.micEnabled == false
             binding.cameraOffImageView.isActivated = roomInfo.cameraEnabled == false
 
