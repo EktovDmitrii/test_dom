500a59d3e9a33be40a64a696043685ea11d57033
Влад Сыргашев <v.syrgashev@secreate.io>
2021-12-18 08:56:35 +0300

    fix(RGSMSD-680): Чат: Можно отправить сообщение с пробелом, после появляется сообщение с ошибкой

--- app/src/main/java/com/custom/rgs_android_dom/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/chat/ChatFragment.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -62,7 +63,7 @@ class ChatFragment : BaseFragment<ChatViewModel, FragmentChatBinding>(R.layout.f
         }
 
         binding.messageEditText.addTextChangedListener {
-            binding.sendMessageImageView.isEnabled = it.toString().isNotEmpty()
+            binding.sendMessageImageView.isEnabled = it.toString().isNotEmpty() && it.toString().isNotBlank()
         }
 
         binding.sendMessageImageView.setOnDebouncedClickListener {
