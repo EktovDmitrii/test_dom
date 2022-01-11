1f5f2b0b06a200b28761092ec932c1376fc8a541
nurzhaussyn <nurzhaussyn.maksat@gmail.com>
2022-01-11 11:18:05 +0500

    пофиксил ui баги карточек услуги (плав и фикс)

--- app/src/main/java/com/custom/rgs_android_dom/ui/catalog/product/single/SingleProductViewModel.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/catalog/product/single/SingleProductViewModel.kt
@@ -3,32 +3,19 @@ package com.custom.rgs_android_dom.ui.catalog.product.single
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.custom.rgs_android_dom.domain.catalog.models.ProductModel
-import com.custom.rgs_android_dom.domain.registration.RegistrationInteractor
 import com.custom.rgs_android_dom.ui.base.BaseViewModel
-import com.custom.rgs_android_dom.ui.client.ClientFragment
-import com.custom.rgs_android_dom.ui.navigation.REGISTRATION
-import com.custom.rgs_android_dom.ui.navigation.ScreenManager
-import com.custom.rgs_android_dom.ui.registration.phone.RegistrationPhoneFragment
 
 class SingleProductViewModel(private val product: ProductModel) : BaseViewModel() {
 
     private val productController = MutableLiveData<ProductModel>()
     val productObserver: LiveData<ProductModel> = productController
 
-    private val showMoreDialogController = MutableLiveData<Unit>()
-    val showMoreDialogObserver: LiveData<Unit> = showMoreDialogController
-
     init {
         productController.value = product
     }
 
-
     fun onBackClick(){
         closeController.value = Unit
-    }
-
-    fun onMoreClick(){
-        showMoreDialogController.value = Unit
     }
 
 }
