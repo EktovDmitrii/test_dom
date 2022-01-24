60a5a923cfd9254b7ddbab68e99a9bbea4066834
Vlad Syrgashev <v.syrgashev@secreate.io>
2022-01-24 12:30:02 +0300

    feat(RGSMSD-929): Добавление недвижимости. Заменить иллюстрацию

--- app/src/main/java/com/custom/rgs_android_dom/ui/client/PropertyItemsAdapter.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/client/PropertyItemsAdapter.kt
@@ -75,7 +75,7 @@ class PropertyItemsAdapter(
                     binding.propertyImageView.setImageResource(R.drawable.ic_type_home)
                 }
                 PropertyType.APARTMENT -> {
-                    binding.propertyImageView.setImageResource(R.drawable.ic_type_apartment)
+                    binding.propertyImageView.setImageResource(R.drawable.ic_type_apartment_124x144px)
                 }
             }
             binding.containerFrameLayout.setOnDebouncedClickListener {
