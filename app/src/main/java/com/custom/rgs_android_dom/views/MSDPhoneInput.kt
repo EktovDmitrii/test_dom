43447b7b957bce1186b3c870735e6a78ad0f06d5
Alexander <aleksandr.yudin@cetelem.ru>
2022-01-26 20:37:37 +0700

    Исправил визуальный эффект при выборе страны после заполнения номера телефона

--- app/src/main/java/com/custom/rgs_android_dom/views/MSDPhoneInput.kt
+++ app/src/main/java/com/custom/rgs_android_dom/views/MSDPhoneInput.kt
@@ -122,6 +122,7 @@ class MSDPhoneInput @JvmOverloads constructor(
         binding.phoneEditText.addTextChangedListener(maskedTextChangedListener)
 
         val hint = makeHint(mask)
+        if (binding.phoneEditText.text.isNullOrBlank())
         binding.hintEditText.hint = hint
         this.hint = hint
 
