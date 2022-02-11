4f185e935bc542b57e13453d4c489e9edefa6cd5
nurzhaussyn <nurzhaussyn.maksat@gmail.com>
2022-02-11 09:43:38 +0600

    Поменял иконку для услуги продукта

--- app/src/main/java/com/custom/rgs_android_dom/ui/catalog/product/service/ServiceFragment.kt
+++ app/src/main/java/com/custom/rgs_android_dom/ui/catalog/product/service/ServiceFragment.kt
@@ -56,7 +56,7 @@ class ServiceFragment : BaseBottomSheetFragment<ServiceViewModel, FragmentServic
                 .transform(RoundedCorners(6.dp(requireContext())))
                 .into(binding.header.iconImageView)
             GlideApp.with(requireContext())
-                .load(GlideUrlProvider.makeHeadersGlideUrl(service.logoLarge))
+                .load(GlideUrlProvider.makeHeadersGlideUrl(service.logoMiddle))
                 .transform(RoundedCorners(16.dp(requireContext())))
                 .into(binding.header.logoImageView)
 
