5cc42fef12faeacd3dbf8ecdcfd0ed6be813adda
RisingMobi <RisingMobi@mail.com>
2022-03-01 16:29:04 +0200

    Changes iv view model module

--- app/src/main/java/com/custom/rgs_android_dom/di/ViewModelModule.kt
+++ app/src/main/java/com/custom/rgs_android_dom/di/ViewModelModule.kt
@@ -60,8 +60,10 @@ import com.custom.rgs_android_dom.ui.policies.insurant.dialogs.PolicyDialogsView
 import com.custom.rgs_android_dom.ui.policies.policy.PolicyViewModel
 import com.custom.rgs_android_dom.ui.property.add.details.files.PropertyUploadDocumentsViewModel
 import com.custom.rgs_android_dom.ui.property.add.select_address.SelectAddressViewModel
+import com.custom.rgs_android_dom.ui.property.delete.DeletePropertyViewModel
 import com.custom.rgs_android_dom.ui.property.document.DocumentViewModel
 import com.custom.rgs_android_dom.ui.property.document.detail_document.DetailDocumentViewModel
+import com.custom.rgs_android_dom.ui.property.info.more.PropertyMoreViewModel
 import com.custom.rgs_android_dom.ui.purchase.*
 import com.custom.rgs_android_dom.ui.purchase.add.agent.AddAgentViewModel
 import com.custom.rgs_android_dom.ui.purchase.add.comment.AddCommentViewModel
@@ -156,4 +158,6 @@ val viewModelModule = module {
     viewModel { parameters -> PolicyViewModel(contractId = parameters.get(),policiesInteractor = get()) }
     viewModel { ChatsViewModel(chatInteractor = get(), registrationInteractor = get()) }
     viewModel { parameters -> CancelOrderViewModel(order = parameters[0], clientInteractor = get()) }
+    viewModel { parameters -> PropertyMoreViewModel(property = parameters.get())}
+    viewModel { parameters -> DeletePropertyViewModel(property = parameters.get(), propertyInteractor = get())}
 }
